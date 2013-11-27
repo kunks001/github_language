64889ce7c077a587baeb9adcd5ce984a542a0408
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-27 19:45:46 +0000

    clean commit

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,8 +3,10 @@ require_relative '../helpers/application'
 
 describe Array do
 	let(:array){ ["one","one","two","one"] }
+	let(:multiple_mode_array){ ["one","one","two","one","two","two","three"] }
 
 	it 'should be able to return the mode of the array' do
 		expect(array.get_mode).to eq "one"
 	end
+	
 end
